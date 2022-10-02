1151acc5f6a38f832dd954c7b3c242978547121b
Githubmancay <111898588+Githubmancay@users.noreply.github.com>
2022-10-02 21:30:48 +0200

    Add files via upload

--- website.py
+++ website.py
@@ -39,6 +39,11 @@ def Grammatik():
     return render_template("Deutsch/Grammatik.html")
 
 
+@app.route("/Deutsch/Wortschatz")
+def Wortschatz():
+    return render_template("Deutsch/Wortschatz.html")
+
+
 ####################################################################################################
 
 
@@ -59,6 +64,11 @@ class AdverbForm2(FlaskForm):
     gap2 = StringField("Mike is quite", validators=[DataRequired()])
     submit2 = SubmitField('Submit')
 
+class AdverbForm3(FlaskForm):
+    gap3 = StringField("Ivan snores", validators=[DataRequired()])
+    submit3 = SubmitField('Submit')
+
+
 # just a helper function to reduce code
 def check_gap(input, expected):
     if input == expected:
@@ -72,14 +82,22 @@ def adverbs():
     message1 = ""
     if form1.validate_on_submit():
         message1 = check_gap(form1.gap1.data, "loudly")
+        
     form2 = AdverbForm2()
     message2 = ""
     if form2.validate_on_submit():
         message2 = check_gap(form2.gap2.data, "quiet")
+    
+    form3 = AdverbForm3()
+    message3 = ""
+    if form3.validate_on_submit():
+        message3 = check_gap(form3.gap3.data, "heavily")
+        
         
     return render_template("Englisch/grammar/adverbs.html", 
                         form1=form1, message1=message1, 
-                        form2=form2, message2=message2)
+                        form2=form2, message2=message2,
+                        form3=form3, message3=message3)
 
 
 
